5ccf9d9d46beefa119f9425475a7ed67ff4b8772
Владимир <baranov-v-k@yandex.ru>
2024-12-25 21:38:06 +0300

    Комет после отката изменений

--- test_file.py
+++ test_file.py
@@ -1,14 +1,1 @@
-def validate_digit_sequence(s):
-    valid_lengths = {13, 16, 18, 19}
-    digits = s.split()
 
-    if len(digits) not in valid_lengths:
-        return False
-
-    for digit in digits:
-        if not digit.isdigit():
-            return False
-
-    return True
-
-print(validate_digit_sequence('1 2 3 4 5 6 7 8 9 10 11 12 13'))
